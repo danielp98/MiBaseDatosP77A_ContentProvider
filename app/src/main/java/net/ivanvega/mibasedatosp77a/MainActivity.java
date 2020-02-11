7b8f39a8b2cd2145bf20e91672727a8c2fc5ed6d
danielp98 <s16120305@alumnos.itsur.edu.mx>
2020-02-11 00:43:53 -0600

    Initial commit

---
+++ app/src/main/java/net/ivanvega/mibasedatosp77a/MainActivity.java
@@ -0,0 +1,72 @@
+package net.ivanvega.mibasedatosp77a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    EditText txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DAOContactos dao = new DAOContactos(this);
+        dao.insert(new Contacto(0, "perronegro",
+                "perronegro@","445"));
+        dao.insert(new Contacto(0, "perroblanco",
+                "perroblanco@","544"));
+         for (Contacto c : dao.getAll()){
+             Toast.makeText(this,
+                     c.usuario,
+                     Toast.LENGTH_SHORT).show();
+         }
+
+
+         lv = findViewById(R.id.lv);
+
+
+        SimpleCursorAdapter adp =
+                new SimpleCursorAdapter(
+                        this,
+                        android.R.layout.simple_list_item_2,
+                        dao.getAllCursor(),
+                        new String[]{"usuario","email"},
+                        new int[]{android.R.id.text1, android.R.id.text2
+                        },
+                        SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
+
+                );
+        lv.setAdapter(adp);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        DAOContactos dao = new DAOContactos(this);
+
+        SimpleCursorAdapter adp =
+                new SimpleCursorAdapter(
+                        this,
+                        android.R.layout.simple_list_item_2,
+                        dao.getAllCursor(),
+                        new String[]{"usuario","email"},
+                        new int[]{android.R.id.text1, android.R.id.text2
+                        },
+                        SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
+
+                );
+        lv.setAdapter(adp);
+    }
+}
